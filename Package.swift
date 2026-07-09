cc2c76bb79a28fa6402eb2afa26657bc41883f45
Filippos Sakellaropoulos <phisakel@gmail.com>
2026-07-09 15:55:18 +0300

    data-model package update to 0.22.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-certificates.git", from: "1.19.0"),
         .package(url: "https://github.com/niscy-eudiw/SwiftCBOR.git", from: "0.6.5"),
-		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-data-model.git", from: "0.14.0"),
+		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-data-model.git", from: "0.22.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
